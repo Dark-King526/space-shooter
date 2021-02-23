2bad0758a9a7dd6d740f3a68e4fe351865926154
Ricardo <913917030@qq.com>
2021-02-24 00:50:27 +0800

    add Assets

---
+++ Assets/EnemyBullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBullet : MonoBehaviour {
+    private float speed = 5;
+    void Start() {
+        Destroy(this.gameObject,5f);
+    }
+	void Update () {
+        transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            other.GetComponent<Hero>().hp--;
+            other.GetComponent<Hero>().UpdatePlayerState();
+        }
+     }
+}
